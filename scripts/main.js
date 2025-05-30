060811b6794f8d66ea524a60480f42e6cae459ec
Ariel Yamien <arielyamien770@gmail.com>
2025-05-30 23:13:23 +0000

    page HTML complet, ajout du style et récuperation des éléments dans le JS .

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,15 @@
+let saisieJoueur = document.getElementById("inputEcriture")
+console.log(saisieJoueur)
+const bouttonValidation = document.getElementById("btnValiderMot")
+console.log(bouttonValidation)
+
+let motPropose = document.querySelector(".zoneProposition")
+console.log(motPropose)
+let score = document.querySelector(".zoneScore span")
+console.log(score)
+
+const bouttonChoix = document.querySelectorAll(".optionSource input")
+console.log(bouttonChoix)
+
 //appel a la fonction lancerJeu pour démarrer le jeu
-lancerJeu()
+// lancerJeu()
